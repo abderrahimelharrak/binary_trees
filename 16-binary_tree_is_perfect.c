c57e38a59f2c016215a0ae3436f235e61d9af43f
abderrahimelharrak <abderrahim.elharrak23@gmail.com>
2023-11-29 15:31:08 -0800

    task_16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -20,10 +20,10 @@ unsigned char is_leaf(const binary_tree_t *node)
  */
 size_t depth(const binary_tree_t *tree)
 {
-	if (tree->parent != NULL)
-		return (1 + depth(tree->parent));
-	else
-		return (0);
+        if (tree->parent != NULL)
+                return (1 + depth(tree->parent));
+        else
+                return (0);
 }
 
 /**
@@ -33,12 +33,12 @@ size_t depth(const binary_tree_t *tree)
  */
 const binary_tree_t *get_leaf(const binary_tree_t *tree)
 {
-	if (is_leaf(tree) == 1)
-		return (tree);
-	if (tree->left)
-		return (get_leaf(tree->left));
-	else
-		return (get_leaf(tree->right));
+        if (is_leaf(tree) == 1)
+                return (tree);
+        if (tree->left)
+                return (get_leaf(tree->left));
+        else
+                return (get_leaf(tree->right));
 }
 
 /**
@@ -49,17 +49,17 @@ const binary_tree_t *get_leaf(const binary_tree_t *tree)
  * Return: success
  */
 int is_perfect_recursive(const binary_tree_t *tree,
-		size_t leaf_depth, size_t level)
+                size_t leaf_depth, size_t level)
 {
-	if (is_leaf(tree))
-		if (level == leaf_depth)
-			return (1);
-		else
-			return (0);
-	if (tree->left == NULL || tree->right == NULL)
-		return (0);
-	return (is_perfect_recursive(tree->left, leaf_depth, level + 1) &&
-		is_perfect_recursive(tree->right, leaf_depth, level + 1));
+        if (is_leaf(tree))
+                if (level == leaf_depth)
+                        return (1);
+                else
+                        return (0);
+        if (tree->left == NULL || tree->right == NULL)
+                return (0);
+        return (is_perfect_recursive(tree->left, leaf_depth, level + 1) &&
+                is_perfect_recursive(tree->right, leaf_depth, level + 1));
 }
 
 /**
@@ -69,7 +69,7 @@ int is_perfect_recursive(const binary_tree_t *tree,
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-		return (0);
-	return (is_perfect_recursive(tree, depth(get_leaf(tree)), 0));
+        if (tree == NULL)
+                return (0);
+        return (is_perfect_recursive(tree, depth(get_leaf(tree)), 0));
 }
