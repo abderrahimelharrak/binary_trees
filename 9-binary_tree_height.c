5de493341570a9b6323026938a8ef785f6b3f382
abderrahimelharrak <abderrahim.elharrak23@gmail.com>
2023-11-29 14:32:45 -0800

    task_9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -12,10 +12,11 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		size_t x = 0, y = 0;
 
 		y = tree->right ? 1 + binary_tree_height(tree->right) : 0;
-		if (x == tree->right)
-			binary_tree_height(tree->right) += 1;
-		else
-			binary_tree_height(tree->right) = 0;
+		
+		if(tree->left)
+			x = binary_tree_height(tree->left) + 1;
+		else 
+			x = 0;
 		return ((x > y) ? x : y);
 	}
 	return (0);
